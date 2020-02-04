ac2717fba850ebed96f941d234383989b5258e88
Praveen Gupta <pvgupta24@gmail.com>
2020-02-04 09:28:36 +0530

    Adds config file and fixes code style

--- main.py
+++ main.py
@@ -1,111 +1,111 @@
-
+"""Simulation for clients in federated-learning."""
 import logging
-import sys
+import os
 
-from google.protobuf import empty_pb2
 import grpc
+import yaml
 
 from fl_round import fl_round_pb2
 from fl_round import fl_round_pb2_grpc
-
 from train import train_on_device
 
-_TIMEOUT_SECONDS = 10000
-address = 'localhost:50051'
+CONFIG_FILE = "config.yaml"
+config = {}
 
 
-_CHUNKER_SIZE = 200
+def get_save_path(fileName):
+    """Directory path to store required files."""
+    return os.path.join(config["device-dir"], fileName)
 
 
 def checkInMessages():
-    msg = fl_round_pb2.CheckInRequest(message='PythonClient')
+    """Checkin request to be sent to selectors."""
+    msg = fl_round_pb2.CheckInRequest(message="PythonClient")
     yield msg
 
-# def get_updates_metadata(weight_updates_file_path, num_batches, chunker_size):
-#     with open(weight_updates_file_path, "rb") as file:
-#         chunk = file.read(chunker_size)
-#         while chunk:
-#             #print(str(chunk))
-#             yield fl_round_pb2.FlData(
-#                 message=fl_round_pb2.Chunk(content=chunk),
-#                 type=fl_round_pb2.FL_CHECKPOINT_UPDATE,
-#                 intVal=num_batches
-#             )
-#             chunk = file.read(chunker_size)
 
 def get_weight_updates(weight_updates_file_path, num_batches, chunker_size):
+    """Gets the checkpoint weights."""
     with open(weight_updates_file_path, "rb") as file:
         chunk = file.read(chunker_size)
         while chunk:
-            #print(str(chunk))
+            # print(str(chunk))
             yield fl_round_pb2.FlData(
                 message=fl_round_pb2.Chunk(content=chunk),
                 type=fl_round_pb2.FL_CHECKPOINT_UPDATE,
-                #intVal=num_batches
+                # intVal=num_batches
             )
             chunk = file.read(chunker_size)
 
     yield fl_round_pb2.FlData(
-        type=fl_round_pb2.FL_CHECKPOINT_WEIGHT,
-        intVal=num_batches
+        type=fl_round_pb2.FL_CHECKPOINT_WEIGHT, intVal=num_batches,
     )
 
+
 def run():
+    """Starts the grpc server to connect to selectors."""
     # NOTE(gRPC Python Team): .close() is possible on a channel and should be
     # used in circumstances in which the with statement does not fit the needs
     # of the code.
+    address = config["address"]
+
     with grpc.insecure_channel(address) as channel:
         client = fl_round_pb2_grpc.FlRoundStub(channel)
 
-        with open(checkpoint_file_path, 'wb') as checkpoint_file:
-            try:
-                # responses = client.CheckIn(empty_pb2.Empty())
-                responses = client.CheckIn(checkInMessages(), _TIMEOUT_SECONDS)
+        try:
+            responses = client.CheckIn(checkInMessages(), config["timeout-in-seconds"])
 
-                # responses.send(fl_round_pb2.CheckInRequest(message = 'TestClientPython'))
-                #print(responses)
-                # received_bytes = bytes()
-                for response in responses:
-                    if(response.type == fl_round_pb2.FL_RECONN_TIME):
-                        print("Could not Check in. Reconnect after " + str(response.intVal) +" seconds")
-                        exit(-1)
-                    #print(response)
-                    checkpoint_file.write(response.message.content)
-                    # print('Received %d bytes...', len(response.chunk))
+            for response in responses:
+                if response.type == fl_round_pb2.FL_INT:
+                    print(
+                        "Could not Check in. Reconnect after "
+                        + str(response.intVal)
+                        + " seconds",
+                    )
+                    # TODO: Reconnect after some time
+                    exit(-1)
 
-                print('------- Checkpoint file downloaded successfully ------')
+                elif response.type == fl_round_pb2.FL_FILES:
+                    with open(
+                        get_save_path(response.filePath), "a+",
+                    ) as checkpoint_file:
+                        checkpoint_file.write(response.message.content)
 
-                
+            print("------- Checkpoint file downloaded successfully ------")
 
-            except grpc.RpcError as rpc_error:
-                print('Encountered a RPC error with ' + address)
-                raise rpc_error
-            
-        # Call helper functions from train.py to train and send back
-        # TODO: Train
-        num_batches, weight_updates_path = train_on_device(data_dir, dataset_id, model_file_path, checkpoint_file_path, weight_updates_file_path)
-        
-        print('----- Completed training on device -----')
+        except grpc.RpcError as rpc_error:
+            print("Encountered a RPC error with " + address)
+            raise rpc_error
+
+        num_batches, weight_updates_path = train_on_device(
+            data_dir,
+            dataset_id,
+            model_file_path,
+            checkpoint_file_path,
+            weight_updates_file_path,
+        )
+
+        print("----- Completed training on device -----")
 
         updated_chunks = get_weight_updates(
-            weight_updates_path, num_batches, _CHUNKER_SIZE)        
-        #print(updated_chunks)
-        response = client.Update(updated_chunks, _TIMEOUT_SECONDS)
-        print('----- Weight updates sent to server -----')
+            weight_updates_path, num_batches, config["chunker-size"],
+        )
+        # print(updated_chunks)
+        response = client.Update(updated_chunks, config["timeout-in-seconds"])
+        print("----- Weight updates sent to server -----")
 
-if __name__ == '__main__':
 
-    if len (sys.argv) != 3 :
-        print("Usage: python main.py <path to directory containing device's content> <dataset id (1, 2, 3, 4, 5)>")
-        sys.exit (1)
+if __name__ == "__main__":
 
-    device_dir = sys.argv[1]
-    dataset_id = int(sys.argv[2])
+    config = yaml.load(open(CONFIG_FILE, "r"))
 
-    data_dir = device_dir + '/data/'
-    checkpoint_file_path = device_dir + '/checkpoint/fl_checkpoint'
-    model_file_path = device_dir + '/model/model.h5'
-    weight_updates_file_path = device_dir + 'weight_updates/fl_weight_updates'
+    device_dir = config["device-dir"]
+    dataset_id = config["dataset-id"]
+
+    data_dir = device_dir + "/data/"
+    checkpoint_file_path = device_dir + "/checkpoint/fl_checkpoint"
+    model_file_path = device_dir + "/model/model.h5"
+    weight_updates_file_path = device_dir + "/weight_updates/fl_weight_updates"
 
     logging.basicConfig()
     run()
--- train.py
+++ train.py
@@ -1,31 +1,14 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
-
-import gzip
+"""Simulates training on client devices."""
 import os
-import sys
-import time
-from six.moves import urllib
-from six.moves import xrange  
-import tensorflow as tf
+
 import numpy as np
-
-import six
-from pathlib import Path
-
-os.environ["CUDA_VISIBLE_DEVICES"]="-1"  
-
-import tensorflow as tf
-import keras
-from tensorflow.keras import layers
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
-from keras import backend as K
 from keras.models import load_model
 
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
-SOURCE_URL = 'https://storage.googleapis.com/cvdf-datasets/mnist/'
-WORK_DIRECTORY = 'data'
+
+SOURCE_URL = "https://storage.googleapis.com/cvdf-datasets/mnist/"
+WORK_DIRECTORY = "data"
 IMAGE_SIZE = 28
 NUM_CHANNELS = 1
 PIXEL_DEPTH = 255
@@ -38,71 +21,68 @@ EVAL_BATCH_SIZE = 64
 EVAL_FREQUENCY = 100  # Number of steps between evaluations.
 
 
-
 def get_mnist_images_and_labels(data_dir):
-    images = np.load(data_dir + '/mnist-images.npy')
-    labels = np.load(data_dir + '/mnist-labels.npy')
+    """Loads the labelled images."""
+    images = np.load(data_dir + "/mnist-images.npy")
+    labels = np.load(data_dir + "/mnist-labels.npy")
     return (images, labels)
 
 
-
 def train_on_device(data_dir, dataset_id, model_path, ckpt_path, weight_updates_path):
-    """
-    Returns (n, weight_updates) after training on local data    
-    """
-    
-    # Store pre-trained model and weights 
+    """Returns (n, weight_updates) after training on local data."""
+    # Store pre-trained model and weights
     old_model = load_model(model_path)
     old_model.load_weights(ckpt_path)
-    
+
     # Initialize model and checkpoint, which are obtained from server
     device_model = load_model(model_path)
     device_model.load_weights(ckpt_path)
-    
-#    print(device_model.summary())
-    
+
+    #    print(device_model.summary())
+
     # Get training data present on device
     train_images, train_labels = get_mnist_images_and_labels(data_dir)
-    
+
     NUM_IMAGES = train_images.shape[0]
     NUM_DIVISIONS = 5
     DIVISION_LEN = NUM_IMAGES // NUM_DIVISIONS
 
-    start_ind = (DIVISION_LEN * (dataset_id-1))
-    end_ind = (DIVISION_LEN * dataset_id)
-    train_images = train_images[start_ind : end_ind]
-    train_labels = train_labels[start_ind : end_ind]
+    start_ind = DIVISION_LEN * (dataset_id - 1)
+    end_ind = DIVISION_LEN * dataset_id
+    train_images = train_images[start_ind:end_ind]
+    train_labels = train_labels[start_ind:end_ind]
 
     # Train model
-    device_model.fit(train_images, train_labels,
-          batch_size=BATCH_SIZE,
-          epochs=1,
-          verbose=1)
-    
+    device_model.fit(
+        train_images, train_labels, batch_size=BATCH_SIZE, epochs=1, verbose=1,
+    )
+
     # Load model to store weight updates
     weight_updates = load_model(model_path)
-    
+
     # Number of batches trained on device
     num_batches = train_images.shape[0] // BATCH_SIZE
-    
+
     # Calculate weight updates
     for i in range(len(device_model.layers)):
-        
+
         # Pre-trained weights
         old_layer_weights = old_model.layers[i].get_weights()
-        
-        # Post-trained weights        
+
+        # Post-trained weights
         new_layer_weights = device_model.layers[i].get_weights()
 
         # Weight updates calculation
-        weight_updates.layers[i].set_weights(num_batches * (np.asarray(new_layer_weights) - np.asarray(old_layer_weights)))
-    
+        weight_updates.layers[i].set_weights(
+            num_batches
+            * (np.asarray(new_layer_weights) - np.asarray(old_layer_weights)),
+        )
+
     #    print("old weights: ",  old_layer_weights)
     #    print("new weights: ",  new_layer_weights)
     #    print("weight updates: ",  weight_updates.layers[i].get_weights())
-    
-    
+
     # Save weight updates
     weight_updates.save_weights(weight_updates_path)
-    
+
     return (num_batches, weight_updates_path)
